54cc1bea133bc070963a719502fa2167766fe9f2
cn <cool_navy@qq.com>
2017-06-01 21:57:01 +0800

    add project group

--- build/premake5.lua
+++ build/premake5.lua
@@ -14,91 +14,6 @@ defines { "LUAINTF_HEADERS_ONLY=0"}
 workspace "lfb"
 	configurations { "Debug", "Release" }
 
-project "liblua"
-	kind "StaticLib"
-	targetdir "../bin/%{cfg.buildcfg}"
-	targetprefix ""  -- linux: lfb.so
-
-	--[[
-	From: https://github.com/SteveKChiu/lua-intf
-	By default LuaIntf expect the Lua library to build under C++.
-	If you really want to use Lua library compiled under C,
-	you can define LUAINTF_LINK_LUA_COMPILED_IN_CXX to 0:
-	--]]
-	-- defines { "LUAINTF_LINK_LUA_COMPILED_IN_CXX=0" }
-
-	files {
-		"../third_party/lua/src/**.c",
-		"../third_party/lua/src/**.h",
-		"../third_party/lua/include/**.h",
-	}
-	includedirs {
-		"../third_party/lua/src",
-		"../third_party/lua-intf",
-		lua_include_dir,
-	}
-	libdirs {
-		-- lib_dir,
-	}
-	flags {
-		"C++11",
-	}
-
-	filter "configurations:Debug"
-		flags { "Symbols" }
-		libdirs { lib_dir .. "/Debug" }
-	filter "configurations:Release"
-		defines { "NDEBUG" }
-		symbols "On"
-		libdirs { lib_dir .. "/Release" }
-	filter {}
-
-	links {
-	}
-
-project "libluaintf"
-	kind "StaticLib"
-	targetdir "../bin/%{cfg.buildcfg}"
-	targetprefix ""  -- linux: lfb.so
-
-	--[[
-	From: https://github.com/SteveKChiu/lua-intf
-	By default LuaIntf expect the Lua library to build under C++.
-	If you really want to use Lua library compiled under C,
-	you can define LUAINTF_LINK_LUA_COMPILED_IN_CXX to 0:
-	--]]
-	-- defines { "LUAINTF_LINK_LUA_COMPILED_IN_CXX=0" }
-
-	files {
-		"../third_party/lua-intf/**.cpp",
-		"../third_party/lua-intf/**.h",
-	}
-	includedirs {
-		"../third_party/lua-intf",
-		"../third_party/lua/src",
-		lua_include_dir,
-	}
-	libdirs {
-		-- lib_dir,
-	}
-	flags {
-		"C++11",
-	}
-
-	filter "configurations:Debug"
-		flags { "Symbols" }
-		libdirs { lib_dir .. "/Debug" }
-	filter "configurations:Release"
-		defines { "NDEBUG" }
-		symbols "On"
-		libdirs { lib_dir .. "/Release" }
-	filter {}
-
-	links {
-		-- "liblua.dll",
-		-- "flatbuffers"
-	}
-
 project "libfblua"
 	kind "SharedLib"
 	targetdir "../bin/%{cfg.buildcfg}"
@@ -145,50 +60,6 @@ project "libfblua"
 		"liblua",
 		"libluaintf",
 		"libflatbuffers",
-	}
-
-project "libflatbuffers"
-	kind "StaticLib"
-	targetdir "../bin/%{cfg.buildcfg}"
-	targetprefix ""
-
-	--[[
-	From: https://github.com/SteveKChiu/lua-intf
-	By default LuaIntf expect the Lua library to build under C++.
-	If you really want to use Lua library compiled under C,
-	you can define LUAINTF_LINK_LUA_COMPILED_IN_CXX to 0:
-	--]]
-	-- defines { "LUAINTF_LINK_LUA_COMPILED_IN_CXX=0" }
-
-	files {
-		"../third_party/flatbuffers/src/**.cpp",
-		"../third_party/flatbuffers/grpc/src/compiler/cpp_generator.cc",
-	}
-	includedirs {
-		"../src",
-		"../third_party/lua-intf",
-		"../third_party/flatbuffers/grpc",
-		"../third_party/flatbuffers/include",
-		lua_include_dir,
-	}
-	libdirs {
-		lib_dir,
-	}
-	flags {
-		"C++11",
-	}
-
-	filter "configurations:Debug"
-		flags { "Symbols" }
-		libdirs { lib_dir .. "/Debug" }
-	filter "configurations:Release"
-		defines { "NDEBUG" }
-		symbols "On"
-		libdirs { lib_dir .. "/Release" }
-	filter {}
-
-	links {
-		-- "flatbuffers"
 	}
 
 project "luac"
@@ -276,4 +147,134 @@ project "flatc"
 		"libflatbuffers"
 	}
 
+group "libs"
+
+project "liblua"
+	kind "StaticLib"
+	targetdir "../bin/%{cfg.buildcfg}"
+	targetprefix ""  -- linux: lfb.so
+
+	--[[
+	From: https://github.com/SteveKChiu/lua-intf
+	By default LuaIntf expect the Lua library to build under C++.
+	If you really want to use Lua library compiled under C,
+	you can define LUAINTF_LINK_LUA_COMPILED_IN_CXX to 0:
+	--]]
+	-- defines { "LUAINTF_LINK_LUA_COMPILED_IN_CXX=0" }
+
+	files {
+		"../third_party/lua/src/**.c",
+		"../third_party/lua/src/**.h",
+		"../third_party/lua/include/**.h",
+	}
+	includedirs {
+		"../third_party/lua/src",
+		"../third_party/lua-intf",
+		lua_include_dir,
+	}
+	libdirs {
+		-- lib_dir,
+	}
+	flags {
+		"C++11",
+	}
+
+	filter "configurations:Debug"
+		flags { "Symbols" }
+		libdirs { lib_dir .. "/Debug" }
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		symbols "On"
+		libdirs { lib_dir .. "/Release" }
+	filter {}
+
+	links {
+	}
+
+project "libluaintf"
+	kind "StaticLib"
+	targetdir "../bin/%{cfg.buildcfg}"
+	targetprefix ""  -- linux: lfb.so
+
+	--[[
+	From: https://github.com/SteveKChiu/lua-intf
+	By default LuaIntf expect the Lua library to build under C++.
+	If you really want to use Lua library compiled under C,
+	you can define LUAINTF_LINK_LUA_COMPILED_IN_CXX to 0:
+	--]]
+	-- defines { "LUAINTF_LINK_LUA_COMPILED_IN_CXX=0" }
+
+	files {
+		"../third_party/lua-intf/**.cpp",
+		"../third_party/lua-intf/**.h",
+	}
+	includedirs {
+		"../third_party/lua-intf",
+		"../third_party/lua/src",
+		lua_include_dir,
+	}
+	libdirs {
+		-- lib_dir,
+	}
+	flags {
+		"C++11",
+	}
+
+	filter "configurations:Debug"
+		flags { "Symbols" }
+		libdirs { lib_dir .. "/Debug" }
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		symbols "On"
+		libdirs { lib_dir .. "/Release" }
+	filter {}
+
+	links {
+		-- "liblua.dll",
+		-- "flatbuffers"
+	}
+
+project "libflatbuffers"
+	kind "StaticLib"
+	targetdir "../bin/%{cfg.buildcfg}"
+	targetprefix ""
+
+	--[[
+	From: https://github.com/SteveKChiu/lua-intf
+	By default LuaIntf expect the Lua library to build under C++.
+	If you really want to use Lua library compiled under C,
+	you can define LUAINTF_LINK_LUA_COMPILED_IN_CXX to 0:
+	--]]
+	-- defines { "LUAINTF_LINK_LUA_COMPILED_IN_CXX=0" }
+
+	files {
+		"../third_party/flatbuffers/src/**.cpp",
+		"../third_party/flatbuffers/grpc/src/compiler/cpp_generator.cc",
+	}
+	includedirs {
+		"../src",
+		"../third_party/lua-intf",
+		"../third_party/flatbuffers/grpc",
+		"../third_party/flatbuffers/include",
+		lua_include_dir,
+	}
+	libdirs {
+		lib_dir,
+	}
+	flags {
+		"C++11",
+	}
+
+	filter "configurations:Debug"
+		flags { "Symbols" }
+		libdirs { lib_dir .. "/Debug" }
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		symbols "On"
+		libdirs { lib_dir .. "/Release" }
+	filter {}
+
+	links {
+		-- "flatbuffers"
+	}
 
